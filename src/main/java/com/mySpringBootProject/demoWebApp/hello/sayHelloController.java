0378be84f28f82adb5062df281323b561b21832a
anol123 <anol.roy123@gmail.com>
2025-01-22 18:33:05 +0530

    Added code for jsp

--- src/main/java/com/mySpringBootProject/demoWebApp/hello/sayHelloController.java
+++ src/main/java/com/mySpringBootProject/demoWebApp/hello/sayHelloController.java
@@ -2,13 +2,12 @@ package com.mySpringBootProject.demoWebApp.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class sayHello {
+public class sayHelloController {
 
     @RequestMapping("say-hello")
-    public String sayHello(){
+    public String sayHelloJsp(){
         return "sayHello";
     }
 }
