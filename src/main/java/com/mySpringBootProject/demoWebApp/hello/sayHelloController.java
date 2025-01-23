bb9090581c59ddd47c68c07f398a61f5a8cef577
anol123 <anol.roy123@gmail.com>
2025-01-23 18:03:16 +0530

    Added login page- jpa

--- src/main/java/com/mySpringBootProject/demoWebApp/hello/sayHelloController.java
+++ src/main/java/com/mySpringBootProject/demoWebApp/hello/sayHelloController.java
@@ -1,7 +1,9 @@
 package com.mySpringBootProject.demoWebApp.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class sayHelloController {
@@ -9,5 +11,12 @@ public class sayHelloController {
     @RequestMapping("say-hello-jsp")
     public String sayHelloJsp(){
         return "sayHello";
+    }
+
+    @RequestMapping("login")
+    public String login_jsp(@RequestParam String name, String password, ModelMap model){
+        model.put("name_1", name);
+        System.out.println("Request param is " + name);
+        return "login";
     }
 }
