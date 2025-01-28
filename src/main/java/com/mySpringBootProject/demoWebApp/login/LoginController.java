55ac75013236b3026dd84737fad530ee263fa16b
anol123 <anol.roy123@gmail.com>
2025-01-28 15:27:09 +0530

    Added GET and POST methods

--- src/main/java/com/mySpringBootProject/demoWebApp/login/LoginController.java
+++ src/main/java/com/mySpringBootProject/demoWebApp/login/LoginController.java
@@ -3,15 +3,22 @@ package com.mySpringBootProject.demoWebApp.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("login-form")
-    public String loginForm(){
+    @RequestMapping(value="login-form",method = RequestMethod.GET)
+    public String gotoLoginPage() {
+        return "loginForm";
+    }
+
+    @RequestMapping(value="login-form", method = RequestMethod.POST)
+    public String loginForm(@RequestParam String name, @RequestParam String password, ModelMap model){
 //        model.put("name", name);
 //        model.put("password", password);
+        //System.out.println("This is printed "+ name);
         return "loginForm";
     }
 }
